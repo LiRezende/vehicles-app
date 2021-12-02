d5e95cf461e1a7e2c6e6a283a37e508ce3fc885f
LiRezende <librusamolin@gmail.com>
2021-12-02 11:07:58 -0300

    Report entity created.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,13 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { UserEntity } from './users/user.entity';
+import { ReportEntity } from './reports/reports.entity';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
     type: 'sqlite',
     database: 'db.sqlite',
-    entities: [UserEntity],
+    entities: [UserEntity, ReportEntity],
     synchronize: true
   }),
     UsersModule, ReportsModule],
