1d0b68233164c383dc583714e98dbc0f5713e81c
LiRezende <librusamolin@gmail.com>
2021-12-06 11:07:02 -0300

    Methods find, findOne, remove and update configured in users service.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -14,4 +14,29 @@ export class UsersService {
 
         return this.repo.save(user);
     }
+
+    find(email: string) {
+        return this.repo.find({ email });
+    }
+
+    findOne(id: number) {
+        return this.repo.findOne(id);
+    }
+
+    async update(id: number, attrs: Partial<UserEntity>) {
+        const user = await this.findOne(id);
+        if(!user) {
+            throw new Error('User not found!');
+        }
+        Object.assign(user, attrs);
+        return this.repo.save(user);
+    }
+
+    async remove(id: number) {
+        const user = await this.findOne(id);
+        if(!user) {
+            throw new Error('User not found!');
+        }
+        return this.repo.remove(user);
+    }
 }
