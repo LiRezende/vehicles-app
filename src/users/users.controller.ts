029bb5d8beb96c1004a0e71ed689842634b36ae4
LiRezende <librusamolin@gmail.com>
2021-12-07 14:55:32 -0300

    GET methods created.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,24 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Param, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
-    constructor(private userService: UsersService) {
+    constructor(private usersService: UsersService) {
     }
 
     @Post('/signup')
     createUser(@Body() body: CreateUserDto) {
-        this.userService.create(body.email, body.password);
+        this.usersService.create(body.email, body.password);
+    }
+
+    @Get()
+    findUsersByEmail(@Query('email') email: string) {
+        this.usersService.find(email);
+    }
+
+    @Get('/:id')
+    findUser(@Param('id') id: string) {
+        return this.usersService.findOne(parseInt(id));
     }
 }
