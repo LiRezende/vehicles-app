62b9e71e5953ce76ecd3bf778271826f8be2c030
LiRezende <librusamolin@gmail.com>
2021-12-07 15:29:55 -0300

    DELETE method created.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Get, Patch, Param, Query } from '@nestjs/common';
+import { Body, Controller, Post, Get, Delete, Patch, Param, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 
@@ -20,5 +20,10 @@ export class UsersController {
     @Get('/:id')
     findUser(@Param('id') id: string) {
         return this.usersService.findOne(parseInt(id));
+    }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id: string) {
+        return this.usersService.remove(parseInt(id));
     }
 }
